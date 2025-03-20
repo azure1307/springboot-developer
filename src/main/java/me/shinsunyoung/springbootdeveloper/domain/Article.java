593d12b4c81f68e444eb29fe9467996b893974cc
azure1307 <azure1307@gmail.com>
2025-03-20 20:50:46 +0900

    0320 블로그 글 작성, 목록 조회 API 구현

--- src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -1,9 +1,14 @@
 package me.shinsunyoung.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity //엔티티로 지정
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
     
     @Id //id 필드를 기본키로 지정
@@ -22,21 +27,5 @@ public class Article {
         this.title = title;
         this.content = content;
     }
-    
-    protected Article() { //기본 생성자
-        
-    }
 
-    //게터
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
 }
