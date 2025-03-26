1174802f2aa7284cef3d45c239a829692134fc0c
azure1307 <azure1307@gmail.com>
2025-03-26 15:31:48 +0900

    0326 블로그 글 생성, 삭제, 수정 뷰 구현

--- src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.domain.Article;
@@ -36,6 +37,19 @@ public class BlogViewController {
 		model.addAttribute("article", new ArticleViewResponse(article));
 
 		return "article";
+	}
+
+	@GetMapping("/new-article")
+	public String newArticle(@RequestParam(name = "id", required = false) Long id, Model model) {
+		//id 값을 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을수도 있음)
+		if (id == null) { //id가 없으면 생성
+			model.addAttribute("article", new ArticleViewResponse());
+		} else { //id가 있으면 수정
+			Article article = blogService.findById(id);
+			model.addAttribute("article", new ArticleViewResponse(article));
+		}
+
+		return "newArticle";
 	}
 
 }
